b18220b77ed9b68cf47d59e303ca1b112e0a8bae
Jaroslav Kotrč <kotrcj@gmail.com>
2013-11-13 23:03:49 +0100

    Added recursion and multiple threads for sorting.

--- src/cz/cuni/mff/spl/sortapp/Main.java
+++ src/cz/cuni/mff/spl/sortapp/Main.java
@@ -73,36 +73,116 @@ public class Main {
 			}
 		}
 	}
+	
+	/** 
+	 * Number of recursion calls of method run which should be called before
+	 * sorting is made.
+	 *  */
+	private static int recursionDepth = 0;
+	
+	/**
+	 * Runs quicksort and mergesort on rows of two identical copies of integer
+	 * matrices.
+	 * 
+	 * @param holder class for storing data
+	 * @param recursion 
+	 */
+	private static void run(SortHolder holder, int recursion){
+		if(recursion < recursionDepth){
+			++recursion;
+			run(holder, recursion);
+		} else {
+			QuickSort.sort(holder.arr1);
+			MergeSort.sort(holder.arr2);
+		}
+	}
+	
+	/**
+	 * Class for thread where sorting is made.
+	 * 
+	 * @author Jaroslav Kotrc
+	 *
+	 */
+	private static class Sorter extends Thread{
+		/** data to sort */
+		private SortHolder holder = new SortHolder();
+		
+		/**
+		 * Creates thread class and initialize its data holder.
+		 * @param cycles number of arrays to sort
+		 * @param numbers number of integers in one array
+		 */
+		public Sorter(int cycles, int numbers){
+			holder.numbers = numbers;
+			holder.cycles = cycles;
+			generate(holder);
+		}
+
+		@Override
+		public void run() {
+			Main.run(holder, 0);
+		}
+		
+	}
 
 	/**
 	 * Generates data of default or specified length and runs quicksort
 	 * and mergesort on them.
-	 * @param args command line arguments can specify number of generated 
-	 * 		integers and sorting cycles. If there are more than 2 arguments
-	 * 		then information about sorting will be printed.
+	 * The command line arguments means:
+	 * 		<ol>
+	 * 		<li>number of integers in one array</li>
+	 * 		<li>number of arrays to sort</li>
+	 * 		<li>number of recursion calls before sorting begins</li>
+	 * 		<li>number of threads for sorting</li>
+	 * 		<li>if any value is present then information about sorting will be printed</li>
+	 * 		</ol>
+	 * 	Arguments can be omitted from back to front.
+	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
+		int threadCnt = 1;
+		int numbers = 1000;
+		int cycles = 10;
 		try{
-			SortHolder holder = new SortHolder();
 			if(args.length >= 1){
-				holder.cycles = Integer.parseInt(args[0]);
+				numbers = Integer.parseInt(args[0]);
 			}
 			if(args.length >= 2){
-				holder.numbers = Integer.parseInt(args[1]);
+				cycles = Integer.parseInt(args[1]);
 			}
-			if(args.length > 2){
-				System.out.printf("Sorting %d integers in %d cycles.\n", holder.numbers, holder.cycles);
+			if(args.length >= 3){
+				recursionDepth = Integer.parseInt(args[2]);
+			}
+			if(args.length >= 4){
+				threadCnt = Integer.parseInt(args[3]);
+			}
+			if(args.length > 4){
+				System.out.printf("Sorting %d integers in %d cycles with %d recursion depth " +
+						"and %d threads.\n", numbers, cycles, recursionDepth, threadCnt);
 			}
 			
-			generate(holder);
-			QuickSort.sort(holder.arr1);
-			MergeSort.sort(holder.arr2);
-			
+			Sorter[] sorters = new Sorter[threadCnt];
+			for(int cnt = 0; cnt < threadCnt; ++cnt){
+				sorters[cnt] = new Sorter(cycles, numbers);
+			}
+			for(int cnt = 0; cnt < threadCnt; ++cnt){
+				sorters[cnt].start();
+			}
+			for(int cnt = 0; cnt < threadCnt; ++cnt){
+				try {
+					sorters[cnt].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+						
 			//System.out.println(holder.toString());
 			
 		} catch(NumberFormatException ex){
 			System.out.println("Usage:\n\t " +
-					"main <number of sorting cycles> <number of integers to sort>");
+					"main <number of sorting cycles> <number of integers to sort>" +
+					" <number of recursion calls before sorting> <number of threads for sorting>" +
+					" <anything if information about sorting should be printed>");
 		}
 		
 
